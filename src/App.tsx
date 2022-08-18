37007e04548d9126c1e3b390ad3d9ecb9dc05f18
AndrewMilovich <triplle@ukr.net>
2022-08-18 14:31:53 +0300

    add db and interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Episode} from "./models/episode.model";
+import {Series} from "./models/series.models";
+import {Movie} from "./models/movie.models";
+import {StreamingService} from "./models/streamingService.model";
+import {User} from "./models/user.model";
+import {genres} from "./db/genre.db";
+import {ShowTv} from "./components/Show/ShowTv";
 
 function App() {
+
+    const episode = new Episode('first_episode', genres[0], new Date(),1.5);
+  const series = new Series('s1', genres[0], new Date(2015, 6, 25), [episode],2);
+  const movie = new Movie('batman', genres[2], new Date(2000, 5, 5),3);
+  const streamingService = new StreamingService('myStream', [episode, series, movie]);
+  const user = new User();
+  user.subscribe(streamingService)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div>
+          <h1>Show</h1>
+          <div>
+            {user.subscriptions[0].streamingService.shows.map(value => <ShowTv key={value.name} show={value}/>)}
+          </div>
+        </div>
+      </div>
   );
 }
 
